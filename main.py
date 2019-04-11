2b665665b5c0951db17420233e70417f02813371
陈伟龙 <>
2019-04-11 14:49:57 +0800

    no message

---
+++ main.py
@@ -0,0 +1,37 @@
+# if False :
+#     print("chenweilong")
+# else:{
+#     print("huyan")
+# }
+
+# num1 = 1
+# num2 = 10
+#
+# print(num1 + num2)
+#
+#
+# name = "chenweilong"
+#
+# print(name)
+#
+# a,b,c = 1,2,"longbaba"
+# print(a)
+# print(b)
+# print(c)
+#
+# var1 = 1
+#
+# print(var1)
+# del var1
+# print(var1)
+
+# s = "chenweilong"
+# print(s[1:5])
+# print(s[0])
+# print(s[2:])
+# print(s + "test")
+# print(s * 10)
+
+list = [1,50,33.66,"longbaba"]
+print(list)
+print(list * 2)
---
+++ python列表.py
@@ -0,0 +1,13 @@
+
+# python列表
+list = ["chenweilong",10,3.1415926,"longbaba"]
+
+tlist = ["tcwl",123456]
+
+print(list)#输出完整列表
+print(list[0])#取列表第一个元素
+print(list[0:2])#取第一个到第二个元素
+
+print(list[1:])#取第二个到末尾
+print(tlist * 2)#输出两边tlist
+print(list + tlist)#打印组合的列表
